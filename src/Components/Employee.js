2cc3e2708c362b8420a353d4895cb90070775592
Amina-Moufakkir <aminamouf22@gmail.com>
2023-02-27 10:07:23 -0500

    all tests passed

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,49 @@
+import { useState, useEffect } from "react";
+
 import PetList from "./PetList";
 import "./Employee.css";
 
-export const Employee = () => {
+export const Employee = ({
+  firstName,
+  lastName,
+  title,
+  prefix,
+  postfix,
+  id,
+}) => {
+  const [pets, setPets] = useState([]);
+  const [showPets, setShowPets] = useState(false);
+
+  const name = `${prefix ? prefix + " " : ""}${firstName} ${lastName}${
+    postfix ? ", " + postfix : ""
+  }`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://vet-api-1.onrender.com/api/pets?employeeId=${id}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setPets(data);
+      } catch (error) {
+        console.error("Error fetching pets:", error);
+      }
+    };
+    fetchData();
+  }, [id]);
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{name}</h3>
+      <h4>{title}</h4>
+      <button onClick={() => setShowPets(!showPets)}>
+        {showPets ? "Hide Pets" : "Show Pets"}
+      </button>
+      {showPets && <PetList pets={pets} />}
     </article>
   );
 };
--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch(
+  //       "https://vet-api-1.onrender.com/api/employees"
+  //     );
+  //     const data = await response.json();
+  //     setEmployees(data);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://vet-api-1.onrender.com/api/employees`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setEmployees(data);
+      } catch (error) {
+        console.error("Error fetching employees:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((employee) => (
+          <Employee key={employee.id} {...employee} />
+        ))}
       </section>
     </main>
   );
