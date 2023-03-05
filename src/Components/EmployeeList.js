8c8e1a92e37a81ed973573d6ea57ea64e289d957
pjungjs <pjungjs@gmail.com>
2023-03-05 18:21:39 -0500

    it can display the employees and the assigned pets.

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [showEmployees, setShowEmployees] = useState([]);
+
+  async function fetchAPI() {
+    const response = await fetch("https://vet-app-0obi.onrender.com/api/employees");
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    fetchAPI()
+      .then((response) => {
+        setShowEmployees(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {
+          showEmployees.map((employee) => {
+            return <Employee 
+              key={employee.id}
+              employee={employee}
+            />
+          })
+        }
       </section>
     </main>
   );
